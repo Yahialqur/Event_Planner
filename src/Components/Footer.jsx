d0826ec3d999b917b158dc3763ba5bcff20896e6
Yahialqur <ymq2003@gmail.com>
2024-08-14 13:29:49 -0400

    Added footer file and intigrated its contents into EventPlanner.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; Event Planner Company. All rights reserved.</p>
+        </footer>
+)}
+
+export default Footer
